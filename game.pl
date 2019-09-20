783803d27078d9455476f504a2d5d127c19fe3a0
Rmolimock <495@holbertonschool.com>
2019-09-20 20:12:35 +0000

    display and greeting

--- game.pl
+++ game.pl
@@ -1,25 +1,41 @@
 #!/usr/bin/perl -w
 my %board;
-for(my $i=0;$i<=9;$i++){
-  $board{$i}=' ';
+for(my $i=1;$i<=9;$i++){
+  $board{$i}=$i;
 }
 $playing=1;
+sub greeting
+{
+	print "\nWelcome to Tic Tac Toe by Nick O'Keefe and Russell Molimock\n\n";
+	print "Enter Player One's Name (X): ";
+	my $player_one = <STDIN>;
+	print "\nEnter Player Two's Name (O): ";
+	my $player_two = <STDIN>;
+	print "---------------\n\n";
+	display();
+	print "To select a square, enter the corresponding number.";
+	print "$player_one: ";
+	$p1_choice = <STDIN>;
+	print $p1_choice "\n";
+}
 sub display
 {
-	print "\n\n-------------------\n";
-	for (my $i=0;$i<9;$i++)
+	print "\n\n-------------\n";
+	for (my $i=1;$i<=9;$i++)
 	{
-		if($i==0||$i==3||$i==6)
+		if($i==1||$i==4||$i==7)
 		{
 			print "|";
 		}
-		$n=$i+1;
-		print "$n: $board{$i} |";
-		if($i==2||$i==5||$i==8)
+		print " $board{$i} |";
+		if($i==3||$i==6||$i==9)
 		{
-			print "\n-------------------\n";
+			print "\n-------------\n";
 		}
 	}
 print "\n\n";
 }
-display();
+greeting();
+for(my $i=0;$i<=9;$i++){
+  $board{$i}=' ';
+}
