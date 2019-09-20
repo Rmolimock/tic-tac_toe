4425e8da2d6acd37ae0c472aad87114452d64398
nokeefe <nokeefe1919@gmail.com>
2019-09-20 18:03:54 +0000

    early stages on input for tic tac toe

---
+++ ttt.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+#tic tac toe game for hackday
+use strict;
+use warnings;
+
+$count = 0
+@board=(1..9);
+print "Please select X or O for Player One: "
+my $player_one = <STDIN>;
+chomp $player_one;
+for ($count = 1; $count < 10; $count++){
+    if ($)
+}
+
+print @board;
